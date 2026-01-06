110d915ea8da0bdfc702dd1922378b441fb66723
Kommisaar <ximingfanox@gmail.com>
2026-01-06 14:59:32 +0800

    feat(translations): 更新翻译文件以支持仪表板和设置面板
    - 添加 DashboardTopCard 上下文，包含欢迎信息、会话控制和星期显示
    - 重命名 GeneralSettingPanel 为 GeneralSettingsPanel 并更新文件路径
    - 重命名 LLMSettingPanel 为 LLMSettingsPanel 并更新文件路径
    - 更新 LLM 设置面板中的 API Format 为 Provider
    - 移除 EmbeddingSettingPanel 和 VectorDBSettingPanel 相关翻译
    - 更新主窗口导航项，添加 Dashboard 标签并调整 Chat 和 Settings 位置
    - 完善中文翻译内容，包括星期名称和日期格式化字符串

--- chronoveil/resources/translations/en_US.ts
+++ chronoveil/resources/translations/en_US.ts
@@ -2,132 +2,175 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
 <context>
-    <name>EmbeddingSettingPanel</name>
+    <name>DashboardTopCard</name>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="16"/>
-        <source>Embedding Settings</source>
+        <location filename="../../views/dashboard_view.py" line="50"/>
+        <location filename="../../views/dashboard_view.py" line="130"/>
+        <source>Welcome back, </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="22"/>
-        <source>Auto</source>
+        <location filename="../../views/dashboard_view.py" line="63"/>
+        <source>Visitor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="26"/>
-        <source>Embedding</source>
+        <location filename="../../views/dashboard_view.py" line="53"/>
+        <location filename="../../views/dashboard_view.py" line="131"/>
+        <source>Start New Session</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="27"/>
-        <source>Select the embedding model to use</source>
+        <location filename="../../views/dashboard_view.py" line="54"/>
+        <location filename="../../views/dashboard_view.py" line="132"/>
+        <source>Resume Last Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="104"/>
+        <source>Monday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="105"/>
+        <source>Tuesday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="106"/>
+        <source>Wednesday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="107"/>
+        <source>Thursday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="108"/>
+        <source>Friday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="109"/>
+        <source>Saturday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="110"/>
+        <source>Sunday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="118"/>
+        <source>Today is {year}:{month}:{day} {weekday}</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>GeneralSettingPanel</name>
+    <name>GeneralSettingsPanel</name>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="28"/>
-        <location filename="../../views/general_setting_panel.py" line="74"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="32"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="84"/>
         <source>General Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="42"/>
-        <location filename="../../views/general_setting_panel.py" line="76"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="46"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="86"/>
         <source>Language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="43"/>
-        <location filename="../../views/general_setting_panel.py" line="77"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="47"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="87"/>
         <source>Select the display language of the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="55"/>
-        <location filename="../../views/general_setting_panel.py" line="78"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="59"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="88"/>
         <source>Theme</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="56"/>
-        <location filename="../../views/general_setting_panel.py" line="79"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="60"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="89"/>
         <source>Choose the visual style of the interface</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>LLMSettingPanel</name>
+    <name>LLMSettingsPanel</name>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="23"/>
-        <location filename="../../views/llm_setting_panel.py" line="96"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="23"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="122"/>
         <source>LLM Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="40"/>
-        <location filename="../../views/llm_setting_panel.py" line="98"/>
-        <source>API Format</source>
+        <location filename="../../views/panels/llm_settings_panel.py" line="40"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="124"/>
+        <source>Provider</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="41"/>
-        <location filename="../../views/llm_setting_panel.py" line="99"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="41"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="125"/>
         <source>Request format used to communicate with the LLM backend</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="51"/>
-        <location filename="../../views/llm_setting_panel.py" line="101"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="51"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="127"/>
         <source>Base URL</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="52"/>
-        <location filename="../../views/llm_setting_panel.py" line="102"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="52"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="128"/>
         <source>API endpoint base URL (e.g., https://api.openai.com/v1)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="62"/>
-        <location filename="../../views/llm_setting_panel.py" line="104"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="62"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="130"/>
         <source>Model Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="63"/>
-        <location filename="../../views/llm_setting_panel.py" line="105"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="63"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="131"/>
         <source>Name of the model to use (e.g., gpt-4o, qwen-max)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="73"/>
-        <location filename="../../views/llm_setting_panel.py" line="107"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="73"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="133"/>
         <source>API Key</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="74"/>
-        <location filename="../../views/llm_setting_panel.py" line="108"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="74"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="134"/>
         <source>Authentication key for the LLM service</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="81"/>
-        <location filename="../../views/llm_setting_panel.py" line="110"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="81"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="136"/>
         <source>Test Connection</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="85"/>
-        <location filename="../../views/llm_setting_panel.py" line="111"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="85"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="137"/>
         <source>Test</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="86"/>
-        <location filename="../../views/llm_setting_panel.py" line="112"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="86"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="138"/>
         <source>Validate your settings by sending a test request</source>
         <translation type="unfinished"></translation>
     </message>
@@ -135,36 +178,20 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../views/main_window.py" line="44"/>
+        <location filename="../../views/main_window.py" line="45"/>
+        <source>Dashboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../views/main_window.py" line="55"/>
+        <location filename="../../views/main_window.py" line="78"/>
         <source>Chat</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../views/main_window.py" line="54"/>
+        <location filename="../../views/main_window.py" line="65"/>
+        <location filename="../../views/main_window.py" line="79"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>VectorDBSettingPanel</name>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="16"/>
-        <source>Vector DB Settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="23"/>
-        <source>Auto</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="27"/>
-        <source>Vector DB</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="28"/>
-        <source>Select the vector database to use</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- chronoveil/resources/translations/zh_CN.ts
+++ chronoveil/resources/translations/zh_CN.ts
@@ -2,177 +2,197 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
-    <name>EmbeddingSettingPanel</name>
+    <name>DashboardTopCard</name>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="16"/>
-        <source>Embedding Settings</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../views/dashboard_view.py" line="50"/>
+        <location filename="../../views/dashboard_view.py" line="130"/>
+        <source>Welcome back, </source>
+        <translation>欢迎回来，</translation>
     </message>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="22"/>
-        <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../views/dashboard_view.py" line="63"/>
+        <source>Visitor</source>
+        <translation>来访者</translation>
     </message>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="26"/>
-        <source>Embedding</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../views/dashboard_view.py" line="53"/>
+        <location filename="../../views/dashboard_view.py" line="131"/>
+        <source>Start New Session</source>
+        <translation>开始新会话</translation>
     </message>
     <message>
-        <location filename="../../views/embedding_setting_panel.py" line="27"/>
-        <source>Select the embedding model to use</source>
-        <translation type="unfinished"></translation>
+        <location filename="../../views/dashboard_view.py" line="54"/>
+        <location filename="../../views/dashboard_view.py" line="132"/>
+        <source>Resume Last Session</source>
+        <translation>继续上次会话</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="104"/>
+        <source>Monday</source>
+        <translation>星期一</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="105"/>
+        <source>Tuesday</source>
+        <translation>星期二</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="106"/>
+        <source>Wednesday</source>
+        <translation>星期三</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="107"/>
+        <source>Thursday</source>
+        <translation>星期四</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="108"/>
+        <source>Friday</source>
+        <translation>星期五</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="109"/>
+        <source>Saturday</source>
+        <translation>星期六</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="110"/>
+        <source>Sunday</source>
+        <translation>星期天</translation>
+    </message>
+    <message>
+        <location filename="../../views/dashboard_view.py" line="118"/>
+        <source>Today is {year}:{month}:{day} {weekday}</source>
+        <translation>今天是 {year}年{month}月{day}日，{weekday}。</translation>
     </message>
 </context>
 <context>
-    <name>GeneralSettingPanel</name>
+    <name>GeneralSettingsPanel</name>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="28"/>
-        <location filename="../../views/general_setting_panel.py" line="74"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="32"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="84"/>
         <source>General Settings</source>
         <translation>一般设置</translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="42"/>
-        <location filename="../../views/general_setting_panel.py" line="76"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="46"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="86"/>
         <source>Language</source>
         <translation>界面语言</translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="43"/>
-        <location filename="../../views/general_setting_panel.py" line="77"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="47"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="87"/>
         <source>Select the display language of the application</source>
         <translation>选择界面显示的语言</translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="55"/>
-        <location filename="../../views/general_setting_panel.py" line="78"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="59"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="88"/>
         <source>Theme</source>
         <translation>主题</translation>
     </message>
     <message>
-        <location filename="../../views/general_setting_panel.py" line="56"/>
-        <location filename="../../views/general_setting_panel.py" line="79"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="60"/>
+        <location filename="../../views/panels/general_settings_panel.py" line="89"/>
         <source>Choose the visual style of the interface</source>
         <translation>选择界面显示的风格</translation>
     </message>
 </context>
 <context>
-    <name>LLMSettingPanel</name>
+    <name>LLMSettingsPanel</name>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="23"/>
-        <location filename="../../views/llm_setting_panel.py" line="96"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="23"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="122"/>
         <source>LLM Settings</source>
         <translation>LLM设置</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="40"/>
-        <location filename="../../views/llm_setting_panel.py" line="98"/>
-        <source>API Format</source>
-        <translation>API格式</translation>
+        <location filename="../../views/panels/llm_settings_panel.py" line="40"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="124"/>
+        <source>Provider</source>
+        <translation>提供商</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="41"/>
-        <location filename="../../views/llm_setting_panel.py" line="99"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="41"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="125"/>
         <source>Request format used to communicate with the LLM backend</source>
         <translation>请求格式，用于与LLM后端进行通信</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="51"/>
-        <location filename="../../views/llm_setting_panel.py" line="101"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="51"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="127"/>
         <source>Base URL</source>
         <translation>基础URL</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="52"/>
-        <location filename="../../views/llm_setting_panel.py" line="102"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="52"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="128"/>
         <source>API endpoint base URL (e.g., https://api.openai.com/v1)</source>
         <translation>API端点基础URL（例如，https://api.openai.com/v1）</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="62"/>
-        <location filename="../../views/llm_setting_panel.py" line="104"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="62"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="130"/>
         <source>Model Name</source>
         <translation>模型名称</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="63"/>
-        <location filename="../../views/llm_setting_panel.py" line="105"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="63"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="131"/>
         <source>Name of the model to use (e.g., gpt-4o, qwen-max)</source>
         <translation>要使用的模型名称（例如，gpt-4o，qwen-max）</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="73"/>
-        <location filename="../../views/llm_setting_panel.py" line="107"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="73"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="133"/>
         <source>API Key</source>
         <translation>API密钥</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="74"/>
-        <location filename="../../views/llm_setting_panel.py" line="108"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="74"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="134"/>
         <source>Authentication key for the LLM service</source>
         <translation>LLM服务认证密钥</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="81"/>
-        <location filename="../../views/llm_setting_panel.py" line="110"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="81"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="136"/>
         <source>Test Connection</source>
         <translation>测试连接</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="85"/>
-        <location filename="../../views/llm_setting_panel.py" line="111"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="85"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="137"/>
         <source>Test</source>
         <translation>测试</translation>
     </message>
     <message>
-        <location filename="../../views/llm_setting_panel.py" line="86"/>
-        <location filename="../../views/llm_setting_panel.py" line="112"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="86"/>
+        <location filename="../../views/panels/llm_settings_panel.py" line="138"/>
         <source>Validate your settings by sending a test request</source>
-        <translation type="unfinished">通过发送测试请求来验证你的设置</translation>
-    </message>
-</context>
-<context>
-    <name>LanguageSettingCard</name>
-    <message>
-        <source>Language</source>
-        <translation type="vanished">界面语言</translation>
+        <translation>通过发送测试请求来验证你的设置</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../views/main_window.py" line="44"/>
+        <location filename="../../views/main_window.py" line="45"/>
+        <source>Dashboard</source>
+        <translation>控制台</translation>
+    </message>
+    <message>
+        <location filename="../../views/main_window.py" line="55"/>
+        <location filename="../../views/main_window.py" line="78"/>
         <source>Chat</source>
         <translation>聊天</translation>
     </message>
     <message>
-        <location filename="../../views/main_window.py" line="54"/>
+        <location filename="../../views/main_window.py" line="65"/>
+        <location filename="../../views/main_window.py" line="79"/>
         <source>Settings</source>
         <translation>设置</translation>
-    </message>
-</context>
-<context>
-    <name>VectorDBSettingPanel</name>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="16"/>
-        <source>Vector DB Settings</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="23"/>
-        <source>Auto</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="27"/>
-        <source>Vector DB</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../views/vector_db_setting_panel.py" line="28"/>
-        <source>Select the vector database to use</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
